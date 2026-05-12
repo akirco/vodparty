073dee0716531165c6a3afabe6075be093da68c1
akirco <earlfx@163.com>
2026-05-12 19:23:37 +0800

    Update  dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -215,7 +215,7 @@ async fn proxy_request(url: String) -> Result<String, String> {
     let res = client
         .get(&url)
         .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36")
-        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;")
+        .header("Accept", "application/json, text/plain, */*")
         .send()
         .await
         .map_err(|e| e.to_string())?;
